bca1d48e2d8409c3e68f22e0b50c17e819e44e68
leon <leona202307@proton.me>
2023-10-05 10:22:15 +0300

    dev: add phpstan

--- node/starter.php
+++ node/starter.php
@@ -4,7 +4,7 @@ $zipFilePath = './snail_starter_pack.zip';
 if (file_exists($zipFilePath)) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Content-Disposition', "Content-Length" );
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Content-Disposition,Content-Length' );
     header('Content-Type: application/zip');
     header('Content-Disposition: attachment; filename="starter_pack.zip"');
     header("Content-Length: " .  filesize($zipFilePath));
